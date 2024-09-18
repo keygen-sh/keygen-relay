68cbf9ea4aec219b94f9e56b75304f0090200f8a
prog-supdex <symeton@gmail.com>
2024-09-18 18:55:44 +0200

    add logger

--- internal/logger/logger.go
+++ internal/logger/logger.go
@@ -1,5 +1,29 @@
 package logger
 
-func Init(verbosity int) {
+import (
+	"io"
+	"log/slog"
+)
 
+func Init(config *Config, output io.Writer) {
+	var programLevel = new(slog.LevelVar)
+
+	switch config.Verbosity {
+	case 0:
+		programLevel.Set(slog.LevelInfo)
+	case 1:
+		programLevel.Set(slog.LevelWarn)
+	case 2:
+		programLevel.Set(slog.LevelDebug)
+	case 3:
+		programLevel.Set(slog.LevelError)
+	default:
+		programLevel.Set(slog.LevelInfo)
+	}
+
+	logger := slog.New(slog.NewTextHandler(output, &slog.HandlerOptions{
+		Level: programLevel,
+	}))
+
+	slog.SetDefault(logger)
 }

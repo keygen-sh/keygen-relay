954caad569c0ef40edc3e7f2fee187778661534f
prog-supdex <symeton@gmail.com>
2024-09-24 19:06:35 +0200

    replace sql.Null to pointer, add created_at and other method, regenerate sqlc

--- internal/cmd/stat_test.go
+++ internal/cmd/stat_test.go
@@ -3,27 +3,30 @@ package cmd_test
 import (
 	"bytes"
 	"context"
-	"database/sql"
 	"errors"
-	"github.com/keygen-sh/keygen-relay/internal/licenses"
 	"testing"
 
 	"github.com/keygen-sh/keygen-relay/internal/cmd"
+	"github.com/keygen-sh/keygen-relay/internal/licenses"
 	"github.com/keygen-sh/keygen-relay/internal/testutils"
 	"github.com/keygen-sh/keygen-relay/internal/ui"
 	"github.com/stretchr/testify/assert"
 )
 
 func TestStatCmd_Success(t *testing.T) {
+	nodeID := int64(123)
+	lastClaimedAt := "2024-01-01T00:00:00Z"
+	lastReleasedAt := "2024-01-05T10:00:00Z"
+
 	manager := &testutils.FakeManager{
 		GetLicenseByIDFn: func(ctx context.Context, id string) (licenses.License, error) {
 			return licenses.License{
 				ID:             "License_1",
 				Key:            "License_Key1",
 				Claims:         5,
-				NodeID:         sql.NullInt64{Int64: 123, Valid: true},
-				LastClaimedAt:  sql.NullString{String: "2024-01-01T00:00:00Z", Valid: true},
-				LastReleasedAt: sql.NullString{String: "2024-01-05T10:00:00Z", Valid: true},
+				NodeID:         &nodeID,
+				LastClaimedAt:  &lastClaimedAt,
+				LastReleasedAt: &lastReleasedAt,
 			}, nil
 		},
 	}

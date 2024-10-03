9374d73a54036f41587a9965c6300342df77cffc
Zeke Gabrielse <ezekg@yahoo.com>
2024-10-03 12:32:12 -0500

    fix build script

--- scripts/build.sh
+++ scripts/build.sh
@@ -28,7 +28,7 @@ main() {
     log_info "building v${VERSION} for ${platform}"
 
     env GOOS="${os}" GOARCH="${arch}" \
-      go build -o "build/${filename}" -ldflags "-X ${PACKAGE}.Version=${VERSION}"
+      go build -o "build/${filename}" -ldflags "-X ${PACKAGE}.Version=${VERSION}" ./cmd/relay
 
     if [ $? -eq 0 ]
     then

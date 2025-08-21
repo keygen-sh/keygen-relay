127db6f969ab490a08594535d012183ec51f52cb
Zeke Gabrielse <ezekg@yahoo.com>
2025-08-21 16:30:04 -0500

    add base for logger

--- internal/logger/logger.go
+++ internal/logger/logger.go
@@ -41,3 +41,39 @@ func Init(config *Config, output io.Writer) {
 
 	slog.SetDefault(logger)
 }
+
+// StringPtr creates an slog attribute for a nullable string pointer.
+// If the pointer is nil, it returns a null value, otherwise the dereferenced string.
+func StringPtr(key string, val *string) slog.Attr {
+	if val == nil {
+		return slog.String(key, "<nil>")
+	}
+	return slog.String(key, *val)
+}
+
+// Int64Ptr creates an slog attribute for a nullable int64 pointer.
+// If the pointer is nil, it returns a null value, otherwise the dereferenced int64.
+func Int64Ptr(key string, val *int64) slog.Attr {
+	if val == nil {
+		return slog.String(key, "<nil>")
+	}
+	return slog.Int64(key, *val)
+}
+
+// IntPtr creates an slog attribute for a nullable int pointer.
+// If the pointer is nil, it returns a null value, otherwise the dereferenced int.
+func IntPtr(key string, val *int) slog.Attr {
+	if val == nil {
+		return slog.String(key, "<nil>")
+	}
+	return slog.Int(key, *val)
+}
+
+// BoolPtr creates an slog attribute for a nullable bool pointer.
+// If the pointer is nil, it returns a null value, otherwise the dereferenced bool.
+func BoolPtr(key string, val *bool) slog.Attr {
+	if val == nil {
+		return slog.String(key, "<nil>")
+	}
+	return slog.Bool(key, *val)
+}
